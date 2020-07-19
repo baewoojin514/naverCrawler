802b003f0200833f6cbc7d0b456693e99282f9a4
baewoojin514 <dnwls514@ajou.ac.kr>
2020-07-19 22:51:23 +0900

    spring boot project is generated

---
+++ naver-crawler/src/test/java/org/personal/Dev/team/navercrawler/NaverCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.personal.Dev.team.navercrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaverCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
