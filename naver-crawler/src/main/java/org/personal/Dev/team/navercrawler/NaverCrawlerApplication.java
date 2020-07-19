8885bb037a2e1178d450d02003e3f5cd348f6a00
baewoojin514 <48473015+baewoojin514@users.noreply.github.com>
2020-07-19 22:52:04 +0900

    spring boot project is generated (#1)

---
+++ naver-crawler/src/main/java/org/personal/Dev/team/navercrawler/NaverCrawlerApplication.java
@@ -0,0 +1,13 @@
+package org.personal.Dev.team.navercrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaverCrawlerApplication.class, args);
+	}
+
+}
